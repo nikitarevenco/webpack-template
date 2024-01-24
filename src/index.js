c404241282987cbaf297e3ce36481f8d3ace599f
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-01-24 10:53:25 +0000

    Add background image

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from "lodash";
+import "./style.css";
+import Cat from "./cat.jpg";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const myCat = new Image();
+  myCat.src = Cat;
+
+  element.appendChild(myCat);
 
   return element;
 }
